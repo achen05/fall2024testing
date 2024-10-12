911882dabe6a1cf4779412c13a598c5c71610f70
achen05 <72044476+achen05@users.noreply.github.com>
2024-10-12 14:01:47 -0700

    Add files via upload

---
+++ slide-show.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+showSlides();
+
+function changeSlide(n) {
+    slideIndex += n;
+    showSlides();
+}
+
+function showSlides() {
+    const slides = document.getElementsByClassName("slide");
+    if (slideIndex >= slides.length) {
+        slideIndex = 0; // Loop back to first slide
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1; // Loop to last slide
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // Hide all slides
+    }
+
+    slides[slideIndex].style.display = "flex"; // Show current slide
+}
